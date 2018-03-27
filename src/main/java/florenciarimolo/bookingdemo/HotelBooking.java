d2a786f4181510f58d8bc653b05af26423f5bf46
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2018-03-27 20:03:04 +0200

    finished part 2

---
+++ src/main/java/florenciarimolo/bookingdemo/HotelBooking.java
@@ -0,0 +1,30 @@
+package florenciarimolo.bookingdemo;
+
+public class HotelBooking {
+    private String hotelName;
+    private double pricePerNight;
+    private int nbOfNights;
+
+    public HotelBooking () {}
+    public HotelBooking(String hotelName, double pricePerNight, int nbOfNights) {
+        this.hotelName = hotelName;
+        this.pricePerNight = pricePerNight;
+        this.nbOfNights = nbOfNights;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public int getNbOfNights() {
+        return nbOfNights;
+    }
+
+    public double getTotalPrice() {
+        return pricePerNight * nbOfNights;
+    }
+}
