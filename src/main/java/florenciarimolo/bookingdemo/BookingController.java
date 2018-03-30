7bdd44a1216bb47be844e86b1370a15ee8bf2bcb
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2018-03-30 20:50:28 +0200

    finished part 6 - jsondoc

--- src/main/java/florenciarimolo/bookingdemo/BookingController.java
+++ src/main/java/florenciarimolo/bookingdemo/BookingController.java
@@ -1,11 +1,17 @@
 package florenciarimolo.bookingdemo;
 
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiPathParam;
+import org.jsondoc.core.pojo.ApiStage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/bookings")
+@Api(name = "Hotel Booking API", description = "Provides a list of methods that manage hotel bookings", stage = ApiStage.RC)
 public class BookingController {
 
     BookingRepository bookingRepository;
@@ -16,23 +22,27 @@ public class BookingController {
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ApiMethod(description = "Get all hotel bookings from the database")
     public List<HotelBooking> getAll() {
         return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
-    public List<HotelBooking> getAffordable(@PathVariable double price) {
+    @ApiMethod(description = "Get all hotel bookings where the price per night is less than the provided value")
+    public List<HotelBooking> getAffordable(@ApiPathParam(name = "price") @PathVariable double price) {
         return bookingRepository.findByPricePerNightLessThan(price);
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public List<HotelBooking> create (@RequestBody HotelBooking hotelBooking) {
+    @ApiMethod(description = "Create a hotel booking and save it to the database")
+    public List<HotelBooking> create(@RequestBody HotelBooking hotelBooking) {
         bookingRepository.save(hotelBooking);
         return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    public List<HotelBooking> remove (@PathVariable long id) {
+    @ApiMethod(description = "Remove the hotel booking with the provided ID from the database")
+    public List<HotelBooking> remove(@ApiPathParam(name = "id") @PathVariable long id) {
         bookingRepository.deleteById(id);
         return bookingRepository.findAll();
     }
--- src/main/java/florenciarimolo/bookingdemo/BookingdemoApplication.java
+++ src/main/java/florenciarimolo/bookingdemo/BookingdemoApplication.java
@@ -1,12 +1,14 @@
 package florenciarimolo.bookingdemo;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJSONDoc
 public class BookingdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookingdemoApplication.class, args);
+    }
 }
