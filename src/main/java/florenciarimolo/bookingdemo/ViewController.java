4a6514cfebe75d9fc2d01280251ccf97ee505781
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2018-03-30 22:22:09 +0200

    finished part 7 - configuration

--- src/main/java/florenciarimolo/bookingdemo/ViewController.java
+++ src/main/java/florenciarimolo/bookingdemo/ViewController.java
@@ -1,5 +1,7 @@
 package florenciarimolo.bookingdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,18 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+    //@Value("${app-mode}")
+    private String appMode;
+
+    public ViewController(Environment environment) {
+        appMode = environment.getProperty("app-mode");
+    }
+
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Florencia Rímolo");
-        model.addAttribute("mode", "development");
+        model.addAttribute("mode", appMode);
         return "index";
     }
 }
